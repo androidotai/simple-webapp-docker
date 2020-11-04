2b6ece7e23be036cc467be7ffa5c0c70c13061aa
androidotai <30710258+androidotai@users.noreply.github.com>
2020-11-04 23:12:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome trigger build again test route and service!"
+    return "Welcome trigger build again test route and service 2!"
 
 @app.route('/how are you')
 def hello():
